74eceb1a756854973c219bfc1f10d2c1079f6b37
srirampotla <42873234+POTLASRIRAM@users.noreply.github.com>
2024-02-20 22:59:09 -0600

    Add files via upload

--- src/test/java/testCases/Test06_SearchProducts.java
+++ src/test/java/testCases/Test06_SearchProducts.java
@@ -5,16 +5,25 @@ import org.testng.annotations.Test;
 
 import pageObjects.SearchProducts;
 import testBase.BaseClass;
+import utilities.DataProviders;
 
 public class Test06_SearchProducts extends BaseClass{
-  @Test(groups = {"Regression","Master"})
-  public void search_products() {
+  @Test(groups = {"Regression","Master"},dataProvider="dp1",dataProviderClass=DataProviders.class)
+  public void search_products(String input) {
 	  try {
 	  SearchProducts sp = new SearchProducts(driver);
-	  sp.input_search(p.getProperty("s_value1"));
+	  sp.input_search(input);
 	  sp.search_submit();
-	  String re_val = sp.returned_value();
-	  Assert.assertEquals("iMac", re_val);
+	  sp.clear_data();
+	  if(input.equalsIgnoreCase("iMac")) {
+		  String re_val = sp.returned_value();
+		  Assert.assertEquals(input, re_val);
+	  }
+	  else if(input.equalsIgnoreCase("fitbit")) {
+		  String nps = sp.nonProductSearch();
+		  System.out.println(nps);
+		  Assert.assertEquals(nps, "There is no product that matches the search criteria.");
+	  }
 	  }
 	  catch(Exception e) {
 		  Assert.fail();
