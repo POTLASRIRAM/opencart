89b800d887be92da4cd39acd5ca08252a8c1ab18
srirampotla <42873234+POTLASRIRAM@users.noreply.github.com>
2024-02-20 22:59:36 -0600

    Add files via upload

--- src/test/java/pageObjects/SearchProducts.java
+++ src/test/java/pageObjects/SearchProducts.java
@@ -15,8 +15,9 @@ public class SearchProducts extends BaseClass{
 	By search = By.xpath("//input[@placeholder='Search']");
 	By search_submit = By.xpath("//button[@class='btn btn-default btn-lg']");
 	By ret_val = By.xpath("//a[normalize-space()='iMac']");
+	By nonprodsearch = By.xpath("p[contains(text(),'There is no product that matches the search criter')]");
 	//Action Methods
-	
+	//p[contains(text(),'There is no product that matches the search criter')]
 	public void input_search(String search_value) {
 		driver.findElement(search).sendKeys(search_value);
 	}
@@ -24,10 +25,18 @@ public class SearchProducts extends BaseClass{
 	public void search_submit() {
 		driver.findElement(search_submit).click();
 	}
+	public void clear_data() {
+		driver.findElement(search).clear();
+	}
 	
 	public String returned_value() {
 		String ret_value = driver.findElement(ret_val).getText();
 		return ret_value;
+	}
+	public String nonProductSearch() {
+		String nonpds = driver.findElement(nonprodsearch).getText();
+		System.out.println(nonpds);
+		return nonpds;
 	}
 	
 	
