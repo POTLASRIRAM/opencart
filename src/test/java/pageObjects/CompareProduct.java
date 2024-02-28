190b2460bd9f9d66aaeba7da6d744c7d37094137
srirampotla <42873234+POTLASRIRAM@users.noreply.github.com>
2024-02-28 09:20:09 -0600

    Add files via upload

---
+++ src/test/java/pageObjects/CompareProduct.java
@@ -0,0 +1,33 @@
+package pageObjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class CompareProduct extends BaseClass{
+
+	public CompareProduct(WebDriver driver) {
+		super(driver);
+		// TODO Auto-generated constructor stub
+	}
+	
+	//Locators
+	By compare_products = By.xpath("//i[@class='fa fa-exchange']");
+	By product_comparison = By.xpath("//a[normalize-space()='product comparison']");
+	
+	//Action
+	public void click_compare() {
+		driver.findElement(compare_products).click();
+	}
+	
+	public void click_product_comparison() {
+		driver.findElement(product_comparison).click();
+	}
+	
+	public String ret_page_title() {
+		String page_title = driver.getTitle();
+		
+		return page_title;
+	}
+
+	
+}
