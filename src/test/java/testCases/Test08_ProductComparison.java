dd9325a13ed5ca008ee1ceca0bcf1deaf95d3a2d
srirampotla <42873234+POTLASRIRAM@users.noreply.github.com>
2024-02-28 09:20:35 -0600

    Add files via upload

---
+++ src/test/java/testCases/Test08_ProductComparison.java
@@ -0,0 +1,31 @@
+package testCases;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import pageObjects.CompareProduct;
+import pageObjects.SearchProducts;
+import testBase.BaseClass;
+
+public class Test08_ProductComparison extends BaseClass{
+  @Test(groups = {"Regression","Master"})
+  public void compare_products() throws Exception{
+	  SearchProducts sp = new SearchProducts(driver);
+	  sp.input_search("iMac");
+	  sp.search_submit();
+	  Thread.sleep(3000);
+	  CompareProduct cp = new CompareProduct(driver);
+	  cp.click_compare();
+	  Thread.sleep(4000);
+	  cp.click_product_comparison();
+	  String res = cp.ret_page_title();
+	  if(res.equals("Product Comparison")) {
+		  Assert.assertTrue(true);
+		  System.out.println(res);
+	  }
+	  else {
+		  Assert.fail();
+	  }
+	  
+  }
+}
