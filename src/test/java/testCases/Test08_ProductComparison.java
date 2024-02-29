43a9d09c377699031a03701ff0d521d70bb2d7a1
srirampotla <42873234+POTLASRIRAM@users.noreply.github.com>
2024-02-28 19:07:11 -0600

    Add files via upload

--- src/test/java/testCases/Test08_ProductComparison.java
+++ src/test/java/testCases/Test08_ProductComparison.java
@@ -8,13 +8,35 @@ import pageObjects.SearchProducts;
 import testBase.BaseClass;
 
 public class Test08_ProductComparison extends BaseClass{
-  @Test(groups = {"Regression","Master"})
+  @Test(priority=1,groups = {"Regression","Master"})
   public void compare_products() throws Exception{
 	  SearchProducts sp = new SearchProducts(driver);
 	  sp.input_search("iMac");
 	  sp.search_submit();
 	  Thread.sleep(3000);
 	  CompareProduct cp = new CompareProduct(driver);
+	  cp.click_compare();
+	  Thread.sleep(4000);
+	  cp.click_product_comparison();
+	  String res = cp.ret_page_title();
+	  if(res.equals("Product Comparison")) {
+		  Assert.assertTrue(true);
+		  System.out.println(res);
+	  }
+	  else {
+		  Assert.fail();
+	  }
+	  
+  }
+  @Test(priority=2,groups = {"Regression","Master"})
+  public void compare_products_listview() throws Exception{
+	  SearchProducts sp = new SearchProducts(driver);
+	  sp.input_search("iMac");
+	  sp.search_submit();
+	  Thread.sleep(3000);
+	  CompareProduct cp = new CompareProduct(driver);
+	  cp.click_listview();
+	  Thread.sleep(4000);
 	  cp.click_compare();
 	  Thread.sleep(4000);
 	  cp.click_product_comparison();
