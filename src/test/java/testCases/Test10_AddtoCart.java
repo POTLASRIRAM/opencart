5933f2ab23a7ee3d4e8b56f74c65e345d5e918ad
srirampotla <42873234+POTLASRIRAM@users.noreply.github.com>
2024-03-12 15:36:26 -0500

    Add files via upload

--- src/test/java/testCases/Test10_AddtoCart.java
+++ src/test/java/testCases/Test10_AddtoCart.java
@@ -13,6 +13,9 @@ import pageObjects.SearchProducts;
 import testBase.BaseClass;
 
 public class Test10_AddtoCart extends BaseClass{
+/*
+=======
+>>>>>>> 9cdb298a3a8f97edcc698f3bf3dc2bb307960ef9
 	@BeforeMethod(groups= {"Regression","Master"})
 	public void login() throws InterruptedException {
 		HomePage hp = new HomePage(driver);
@@ -33,7 +36,12 @@ public class Test10_AddtoCart extends BaseClass{
 		 lg.logout();
 		 System.out.println("Logout Successful");
 	}
+<<<<<<< HEAD
+
+  @Test(priority =1, groups = {"Regression","Sanity","Master"})
+=======
   @Test(groups = {"Regression","Sanity","Master"})
+>>>>>>> 9cdb298a3a8f97edcc698f3bf3dc2bb307960ef9
   public void test_add_to_cart()throws Exception {
 	  SearchProducts sp = new SearchProducts(driver);
 	  sp.input_search("imac");
@@ -51,4 +59,61 @@ public class Test10_AddtoCart extends BaseClass{
 		  Assert.fail();
 	  }
   }
+<<<<<<< HEAD
+  
+  @Test(priority =2, groups = {"Regression","Sanity","Master"})
+  public void test_addtocart_viewcart()throws Exception {
+	  SearchProducts sp = new SearchProducts(driver);
+	  sp.input_search("imac");
+	  sp.click_submit();
+	  Thread.sleep(4000);
+	  AddtoCart ac = new AddtoCart(driver);
+	  ac.click_addto_cart();
+	  Thread.sleep(4000);
+	  ac.click_add_to_cart();
+	  Thread.sleep(4000);
+	  ac.click_view_cart();
+	  boolean pdn = ac.re_pd_name();
+	  if(pdn == true) {
+		  Assert.assertTrue(true);
+	  }
+	  else {
+		  Assert.fail();
+	  }
+  }
+  
+  @Test(priority =3, groups = {"Regression","Sanity","Master"})
+  public void test_addtocart_showdesktops()throws Exception {
+	  AddtoCart ac = new AddtoCart(driver);
+	  ac.click_desktop();
+	  ac.click_show_desktops();
+	  ac.click_mac();
+	  Thread.sleep(4000);
+	  ac.add_cart();
+	  ac.click_shoppingcart_test3();
+	  Thread.sleep(4000);
+	  boolean pdn = ac.get_pdname_test3();
+	  if(pdn == true) {
+		  Assert.assertTrue(true);
+	  }
+	  else {
+		  Assert.fail();
+	  }
+  }
+  */
+  @Test(priority =4, groups = {"Regression","Sanity","Master"})
+  public void test_addfeaturedproducts_tocart()throws Exception {
+	  AddtoCart ac = new AddtoCart(driver);
+	  ac.addcart_macbook();
+	  ac.click_shopping_cart();
+	  Thread.sleep(4000);
+	  boolean pdn = ac.ret_macbook();
+	  if(pdn == true) {
+		  Assert.assertTrue(true);
+	  }
+	  else {
+		  Assert.fail();
+	  }
+  }
+
 }
