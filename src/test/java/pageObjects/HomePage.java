0f1b48dedfd5be5d208f746612948e8a1de4f38a
srirampotla <42873234+POTLASRIRAM@users.noreply.github.com>
2024-03-14 20:11:59 -0500

    Add files via upload

--- src/test/java/pageObjects/HomePage.java
+++ src/test/java/pageObjects/HomePage.java
@@ -14,19 +14,32 @@ public class HomePage extends BaseClass{
 	//Locators
 	
 	By myAccount = By.xpath("//span[normalize-space()='My Account']");
+	By myAccountbutton = By.xpath("//ul[@class='dropdown-menu dropdown-menu-right']//a[normalize-space()='My Account']");
 	By register = By.xpath("//a[normalize-space()='Register']");
 	By login = By.xpath("//a[normalize-space()='Login']");
+	By desktop = By.xpath("//a[normalize-space()='Desktops']");
+	By alldesktops = By.xpath("//a[normalize-space()='Show All Desktops']");
 	
 	//Action methods
 	
 	public void clickmy_Account() {
 	     driver.findElement(By.xpath("//span[normalize-space()='My Account']")).click();
 	}
+	public void click_myaccount_button() {
+		driver.findElement(myAccountbutton).click();
+	}
 	public void clickRegister() {
 		driver.findElement(By.xpath("//a[normalize-space()='Register']")).click();
 	}
 	public void clickLogin() {
 		driver.findElement(login).click();
+	}
+	
+	public void clickdesktop() {
+		driver.findElement(desktop).click();
+	}
+	public void clickalldesktops() {
+		driver.findElement(alldesktops).click();
 	}
 	
 
--- src/test/java/pageObjects/WishList.java
+++ src/test/java/pageObjects/WishList.java
@@ -16,7 +16,62 @@ public class WishList extends BaseClass{
 	By wishlist1 = By.xpath("//a[normalize-space()='wish list']");
 	By test1_pdname = By.xpath("//a[normalize-space()='iMac']");
 	
+	//Test2
+	By modify_wishlist = By.xpath("//a[normalize-space()='Modify your wish list']");
+	By remove_wl_product = By.xpath("//i[@class='fa fa-times']");
+	By modify_res = By.xpath("//i[@class='fa fa-times']");
+	By my_wishlist = By.xpath("//h2[normalize-space()='My Wish List']");
+	By wishlist2 = By.xpath("//span[normalize-space()='Wish List (1)']");
+	By wishlist3 = By.xpath("//a[normalize-space()='Wish List']");
+	By successful_remove = By.xpath("//*[@id=\"content\"]/h2");
+	
 	//Action Methods
+	//Test2
+	
+	public boolean click_top_wishlist() {
+		boolean output = false;
+		driver.findElement(wishlist2).click();
+		String name = driver.findElement(my_wishlist).getText();
+		if(name.equals("My Wish List")) {
+			output = true;
+		}
+		else {
+			output = false;
+		}
+		return output;
+	}
+	public boolean click_footer_wishlist() {
+		boolean output = false;
+		driver.findElement(wishlist3).click();
+		String name = driver.findElement(my_wishlist).getText();
+		if(name.equals("My Wish List")) {
+			output = true;
+		}
+		else {
+			output = false;
+		}
+		return output;
+	}
+	public void click_modify_wishlist() {
+		driver.findElement(modify_wishlist).click();
+	}
+	public void click_remove_wishlist_item() {
+		driver.findElement(remove_wl_product).click();
+	}
+	
+	public boolean ret_remove_output() {
+		boolean output = false;
+		String otpt = driver.findElement(successful_remove).getText();
+		System.out.println(otpt);
+		if(otpt.equals("My Wish List")) {
+			output = true;
+		}
+		else {
+			output = false;
+		}
+		return output;
+	}
+	
 	//Test1
 	public void click_add_to_wishlist() {
 		driver.findElement(add_to_wishlist).click();
