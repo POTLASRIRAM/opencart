d58948b98c246d089b2a7a41f9a212fd455e8cb2
srirampotla <42873234+POTLASRIRAM@users.noreply.github.com>
2024-03-15 22:13:06 -0500

    Add files via upload

--- src/test/java/pageObjects/WishList.java
+++ src/test/java/pageObjects/WishList.java
@@ -1,7 +1,10 @@
 package pageObjects;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class WishList extends BaseClass{
 
@@ -25,9 +28,57 @@ public class WishList extends BaseClass{
 	By wishlist3 = By.xpath("//a[normalize-space()='Wish List']");
 	By successful_remove = By.xpath("//*[@id=\"content\"]/h2");
 	
+	//Test3
+    By add_imac_to_wishlist = By.xpath("//*[@id=\"content\"]/div[3]/div/div/div[2]/div[2]/button[2]");
+    By wishlist_count = By.xpath("//*[@id=\"content\"]/div[1]/table/tbody/tr");
+    
+    //Test4
+    By bredcrumb = By.xpath("//*[@id=\"account-wishlist\"]/ul/li");
+    
+    
 	//Action Methods
+    //Test4
+    public boolean get_brudcrumb_info() {
+    	boolean count = false;
+    	List<WebElement> items = driver.findElements(bredcrumb);
+    	for(WebElement x : items) {
+    		System.out.println(x.getText());
+    	}
+    	if(items.size() == 3) {
+    		count = true;
+    	}
+    	else {
+    		count = false;
+    	}
+    	return count;
+    	
+    }
+    
+    //Test3
+    public void click_imac_to_wishlist() {
+    	driver.findElement(add_imac_to_wishlist).click();
+    	/*driver.findElement(add_imac_to_wishlist).click();
+    	driver.findElement(add_imac_to_wishlist).click();
+    	driver.findElement(add_imac_to_wishlist).click();
+    	*/
+    }
+    public void click_test3_wishlist() {
+    	driver.findElement(add_to_wishlist).click();
+    }
+    public boolean ret_product_count() {
+    	boolean res= false;
+    	int count = driver.findElements(wishlist_count).size();
+    	System.out.println(count);
+    	if(count == 1) {
+    		res = true;
+    	}
+    	else {
+    		res = false;
+    	}
+    	return res;
+    }
+    
 	//Test2
-	
 	public boolean click_top_wishlist() {
 		boolean output = false;
 		driver.findElement(wishlist2).click();
