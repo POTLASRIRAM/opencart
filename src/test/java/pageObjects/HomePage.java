6eeeb5b55de761e279193ff670c48ba36489a40a
srirampotla <42873234+POTLASRIRAM@users.noreply.github.com>
2024-03-23 19:46:48 -0500

    Add files via upload

--- src/test/java/pageObjects/HomePage.java
+++ src/test/java/pageObjects/HomePage.java
@@ -1,9 +1,20 @@
 package pageObjects;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+
+import ru.yandex.qatools.ashot.comparison.ImageDiff;
+import ru.yandex.qatools.ashot.comparison.ImageDiffer;
 
 public class HomePage extends BaseClass{
 	
@@ -23,9 +34,12 @@ public class HomePage extends BaseClass{
 	By alldesktops = By.xpath("//a[normalize-space()='Show All Desktops']");
 	
 	//Test2
-	WebElement desktops = (WebElement) By.xpath("//a[normalize-space()='Desktops']");
-	WebElement pc = (WebElement) By.xpath("//a[normalize-space()='PC (0)']");
+	WebElement desktops = driver.findElement(By.xpath("//a[normalize-space()='Desktops']"));
+	WebElement pc = driver.findElement(By.xpath("//a[normalize-space()='PC (0)']"));
 	By pc_continue = By.xpath("//a[@class='btn btn-primary']");
+	
+	//Test3
+	By homepageimage = By.xpath("//img[@title='Your Store']");
 	
 	//Action methods
 	
@@ -57,6 +71,43 @@ public class HomePage extends BaseClass{
 	Actions act = new Actions(driver);
 	public void test2_Hover_Desktop_pc() {
 		act.moveToElement(desktops).moveToElement(pc).click().perform();
+	}
+	public void test2_Pc_Continue() {
+		driver.findElement(pc_continue).click();
+	}
+	
+	//Test3
+	public void test3_click_HomePage() {
+		driver.findElement(homepageimage).click();
+	}//*[@id="slideshow0"]/div/div[" + index + "]
+	public void test3_get_images() throws Exception {
+		//String dynamicXPath = String.format("//*[@id=\"slideshow0\"]/div/div[\" + index + \"]");
+		//driver.findElement(By.xpath(dynamicXPath));
+		//String altText = "iPhone 6"; // or "MacBookAir" depending on which one you want to locate
+
+		//String dynamicXPath = String.format("//div[@class='swiper-slide text-center swiper-slide-active']//img[@alt='%s']", altText);
+		
+		File src = driver.findElement(By.xpath("//div[@class='swiper-slide text-center swiper-slide-duplicate swiper-slide-active']//img[@alt='iPhone 6']")).getScreenshotAs(OutputType.FILE);
+    	//File src = ts.getScreenshotAs(OutputType.FILE);
+    	File trg = new File("D:\\Selenium with Java\\opencart\\screenshots\\Testscreenshots\\expected_image.jpg");
+    	FileUtils.copyFile(src, trg);
+    	// Load expected image
+        BufferedImage expectedImage = ImageIO.read(new File("D:\\\\Selenium with Java\\\\opencart\\\\screenshots\\\\Testscreenshots\\\\expected_image.jpg"));
+
+        // Load actual image
+        BufferedImage actualImage = ImageIO.read(src);
+
+        // Compare images
+        ImageDiffer imgDiff = new ImageDiffer();
+        ImageDiff diff = imgDiff.makeDiff(expectedImage, actualImage);
+        boolean imageotpt = false;
+        // Analyze difference
+        if (diff.hasDiff()) {
+        	imageotpt = false;
+        }
+        else {
+        	imageotpt = true;
+        }
 	}
 	
 
