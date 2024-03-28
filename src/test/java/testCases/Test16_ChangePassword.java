151b2c5730023dfcf239eddf966a4206e8fd3aca
srirampotla <42873234+POTLASRIRAM@users.noreply.github.com>
2024-03-27 19:55:44 -0500

    Add files via upload

--- src/test/java/testCases/Test16_ChangePassword.java
+++ src/test/java/testCases/Test16_ChangePassword.java
@@ -5,15 +5,13 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import pageObjects.ChangePassword;
 import pageObjects.HomePage;
 import pageObjects.LoginPage;
 import pageObjects.Logout;
-import pageObjects.MyAccountInformation;
-import pageObjects.MyAccountPage;
-import pageObjects.ShoppingCart;
 import testBase.BaseClass;
 
-public class Test15_MyAccountInformation extends BaseClass{
+public class Test16_ChangePassword extends BaseClass{
 	@BeforeMethod(groups= {"Regression","Master"})
 	public void login() throws InterruptedException {
 		HomePage hp = new HomePage(driver);
@@ -35,14 +33,14 @@ public class Test15_MyAccountInformation extends BaseClass{
 		 System.out.println("Logout Successful");
 	}
 	/*
-  @Test(priority =1,groups = {"Sanity","regression","Master"})
-  public void test1_my_account_information() throws Exception{
-	  ShoppingCart sc = new ShoppingCart(driver);
-	  sc.test1_click_sitemap();
-	  Thread.sleep(3000);
-	  MyAccountInformation mai = new MyAccountInformation(driver);
-	  mai.test1_click_account_information();
-	  boolean res = mai.test1_ret_validation_output();
+  @Test(priority = 1,groups = {"Sanity","Regression","Master"})
+  public void test1_changepsswd() throws Exception{
+	  ChangePassword cp = new ChangePassword(driver);
+	  cp.test1_click_changepassword();
+	  cp.test1_input_password();
+	  cp.test1_input_confirm_password();
+	  cp.test1_continue();
+	  boolean res = cp.test1_ret_output();
 	  if(res == true) {
 		  Assert.assertTrue(true);
 	  }
@@ -50,19 +48,19 @@ public class Test15_MyAccountInformation extends BaseClass{
 		  Assert.fail();
 	  }
   }*/
-	@Test(priority =2,groups = {"Sanity","regression","Master"})
-	  public void test2_my_account_information() throws Exception{
-		MyAccountPage map = new MyAccountPage(driver);
-		map.click_myaccount();
-		MyAccountInformation mai = new MyAccountInformation(driver);
-		  mai.test2_click_my_account();
-		  mai.test2_click_edit_account();
-		  boolean res = mai.test2_mandatory_field();
-		  if(res == true) {
+	
+	@Test(priority = 2,groups = {"Sanity","Regression","Master"})
+	  public void test2_changepsswd() throws Exception{
+		  ChangePassword cp = new ChangePassword(driver);
+		  cp.test1_click_changepassword();
+		  boolean res_pswd = cp.test2_get_password_type();
+		  boolean res_cnfpswd = cp.test2_get_confirmpassword_type();
+		  if(res_pswd == true && res_cnfpswd == true) {
 			  Assert.assertTrue(true);
 		  }
 		  else {
 			  Assert.fail();
 		  }
+		  
 	  }
 }
