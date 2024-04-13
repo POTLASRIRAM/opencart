568f0c95138e509a1df1bfedc087547192b74111
srirampotla <42873234+POTLASRIRAM@users.noreply.github.com>
2024-04-12 22:02:32 -0500

    Add files via upload

---
+++ src/test/java/pageObjects/RecurringPayments.java
@@ -0,0 +1,40 @@
+package pageObjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class RecurringPayments  extends BaseClass{
+
+	public RecurringPayments(WebDriver driver) {
+		super(driver);
+		// TODO Auto-generated constructor stub
+	}
+	
+	//Locators
+	//Test1
+	By recurring_payments = By.xpath("//a[@class='list-group-item'][normalize-space()='Recurring payments']");
+	By cont = By.xpath("//a[@class='btn btn-primary']");
+	
+	//Action Methods
+	public void test1_click_recurring_methods() {
+		driver.findElement(recurring_payments).click();
+	}
+	
+	public void test1_click_continue() {
+		driver.findElement(cont).click();
+	}
+	
+	public boolean test1_return_otpt() {
+		boolean res = false;
+		String title = driver.getTitle();
+		if(title.equals("My Account")) {
+			res = true;
+		}
+		else {
+			res = false;
+		}
+		return res;
+	}
+	//)
+
+}
