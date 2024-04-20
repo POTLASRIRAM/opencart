d7aa5d63afbb078a3883628625bb76bcc52e3415
srirampotla <42873234+POTLASRIRAM@users.noreply.github.com>
2024-04-19 23:29:11 -0500

    Add files via upload

---
+++ ContactUs.java
@@ -0,0 +1,14 @@
+package pageObjects;
+
+import org.openqa.selenium.WebDriver;
+
+public class ContactUs extends BaseClass{
+
+	public ContactUs(WebDriver driver) {
+		super(driver);
+		// TODO Auto-generated constructor stub
+	}
+	
+	//Locators
+
+}
